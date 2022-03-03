6f7c7f5d37bb96956c97707a43a7006ce04b7714
taro <torataro141@gmail.com>
2022-03-03 15:23:40 +0900

    first commit

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container"><nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">DoSeeThis</a>
+    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="Navber">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/profile">プロフィール</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/setting">設定</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ドロップダウン
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">メニュー1</a></li>
+            <li><a class="dropdown-item" href="#">メニュー2</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">その他</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">無効</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input type="search" class="form-control me-2" placeholder="検索..." aria-label="検索...">
+        <button type="submit" class="btn btn-outline-success flex-shrink-0">検索</button>
+      </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale())}}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-widthm initial-scale=1">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Vue Laravel SPA') }}</title>
+
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <header-component></header-component>
+            <example-component></example-component>
+            <router-view></router-view>
+        </div>
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+    </body>
+</html>
