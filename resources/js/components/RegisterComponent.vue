8f45447b704cc4e9f6540bde43cbeabededcd2a1
taro <torataro141@gmail.com>
2022-03-03 15:51:10 +0900

    header create

---
+++ resources/js/components/RegisterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <form >
+                    <div class="form-group row">
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,6 @@
     <body>
         <div id="app">
             <header-component></header-component>
-            <example-component></example-component>
             <router-view></router-view>
         </div>
         <script src="{{ mix('/js/app.js') }}" defer></script>
