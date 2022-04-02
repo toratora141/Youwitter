a658925dcd0689531645fc23539a506a36a97d94
taro <torataro141@gmail.com>
2022-04-02 23:31:53 +0900

    rename follow.php

--- app/Models/follow.php
+++ app/Models/follow.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class follow extends Model
+class Follow extends Model
 {
     use HasFactory;
 
