7464d60ea99cc22169e201c6d354a0af8553d872
taro <torataro141@gmail.com>
2022-04-07 01:31:30 +0900

    a little fix on home(vue)

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -54,10 +54,10 @@
                     this.follows.forEach(follow => {
                         this.$nextTick(()=>{
                             var user = follow.user_id;
-                            if(follow.video_list === null || follow.video_list === undefined){
+                            if(follow.video_lists === null || follow.video_lists === undefined){
                                 this.$refs.movieList[count].fetch(null, null);
                             }else{
-                                this.$refs.movieList[count].fetch(follow.video_list, follow.video_list.videos);
+                                this.$refs.movieList[count].fetch(follow.video_lists, follow.video_lists.videos);
                             }
                             count++;
                         });
