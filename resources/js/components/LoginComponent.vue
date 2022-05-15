af15972e85efc5265bc52ebcc9c099bff7895b21
taro <torataro141@gmail.com>
2022-05-15 09:00:13 +0900

    a little fix view

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -9,10 +9,10 @@
                 <button class="btn btn-secondary">アカウント作成</button>
             </router-link>
         </div>
-        <div class="row justify-content-center w-100">
-            <div class="col-sm-6">
+        <div class="justify-content-center w-100">
+            <div class="col-sm-6 p-2">
                 <form v-on:submit.prevent="login">
-                    <div class="form-group row" >
+                    <div class="form-group" >
                         <label for="account-name" class="col-sm-3 col-form-label w-100">ID</label>
                         <input type="text"
                             class="col-sm-9 form-control"
